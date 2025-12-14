9f73f289389175fbe7bbda9e0ac2abd2d6828400
Arturo <artuleg@gmail.com>
2025-12-14 12:09:52 +0100

    refactor: simplify .env file path configuration

--- src/core/config.py
+++ src/core/config.py
@@ -160,7 +160,7 @@ class Settings(BaseSettings):
     """
 
     model_config = SettingsConfigDict(
-        env_file=str(Path(__file__).parent.parent.parent / ".env"),
+        env_file=".env",
         env_file_encoding="utf-8",
         case_sensitive=False,
         extra="ignore",
